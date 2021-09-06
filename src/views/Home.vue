fc6e4eef9eb8e106e3b1be94572c48ad971e5f4d
Bilal Haidar <bhaidar@gmail.com>
2021-09-06 18:01:46 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Home">
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet.
+    </p>
+
+    <h2>Amet sit</h2>
+    <p>
+      Eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet.
+    </p>
+  </div>
+</template>
+
+<script>
+import LayoutDefault from '../layouts/LayoutDefault.vue';
+
+export default {
+  name: `Home`,
+  created() {
+    this.$emit(`update:layout`, LayoutDefault);
+  },
+};
+</script>
